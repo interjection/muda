7c5f287f344feeea3a891d3142ee60f674b36092
Michael Parks <mparks@tkware.info>
2015-04-12 20:03:53 -0600

    Fix syntax bug, non-image uploading, formatting

--- muda.rb
+++ muda.rb
@@ -4,7 +4,7 @@ require 'trollop'
 require 'find'
 
 opts = Trollop::options do
-  version "muda.rb v1.2"
+  version "muda.rb v1.2.1"
   banner <<-EOS
 muda.rb is a simple directory dumper - give it a thread, board, and directory to dump, and it does the rest. 
 
@@ -13,7 +13,7 @@ Usage: muda.rb [flags]
 Example invocation: ./muda.rb --thread 123456 --board b --directory /home/anon/files
 
 Flags are:
-EOS
+  EOS
   opt :thread, "Thread ID (required)", :type => :string
   opt :board, "Board ID w/o slashes (required)", :type => :string
   opt :directory, "Directory to dump (required)", :type => :string, :default => '.'
@@ -32,6 +32,7 @@ EOS
   opt :useragent, "User-agent to use", :type => :string, :default => "Mozilla/5.0 (Gentoo GNU/Linux 4.0; GNU/Emacs 24.5) #{version}"
   opt :userflag, "Flag to use", :type => :integer
   opt :verbose, "Verbose mode"
+  opt :nonimages, "Allow uploading of non-image files"
 end
 
 Trollop::die :thread, "Must be set" unless opts[:thread]
@@ -42,111 +43,115 @@ Trollop::die :site, "Invalid - should be in the form of 'http://site.com/" unles
 Trollop::die :maxfiles, "Max files cannot be greater than five" if opts[:maxfiles] > 5
 
 #Image Loading
-puts "Searching for image files in #{opts[:directory]}"
+puts "Searching for #{opt[:nonimages] ? 'image' : 'ANY'} files in #{opts[:directory]}"
 files = []
 Find.find(opts[:directory]) do |path|
-  files << path if path =~ /.+\.(jpg|jpeg|png|gif|bmp)$/i
-end
-puts "Found #{files.size} files to dump"
-
-files.shuffle! if opts[:random]
-
-#Web interaction
-agent = Mechanize.new
-agent.user_agent = opts[:useragent]
-#If you are having trouble dumping on a site that isn't 8chan, this is probably part of
-#what needs to be modified. This should be the same for all vichan boards, but it absolutely
-#won't work on Futaba-likes.
-thread = opts[:site] + opts[:board] + "/res/" + opts[:thread] + ".html"
-posthandler = opts[:site] + "post.php"
-
-puts "Thread is: #{thread}" if opts[:verbose]
-puts "User-agent is #{opts[:useragent]}" if opts[:verbose]
-puts "Password is: #{opts[:password]}" if opts[:verbose]
-puts "Files per post: #{opts[:maxfiles]}" if opts[:verbose]
-puts "Name is #{opts[:name]}" if opts[:verbose]
-puts "Email is #{opts[:email]}" if opts[:verbose]
-puts "Subject is #{opts[:subject]}" if opts[:verbose]
-
-errors = 0 #running error count
-started = false #don't wait the first time
-
-puts "Starting run with #{opts[:delay]} seconds between posts"
-puts "===== " + Time.now.to_s + " ====="
-
-begin
-  agent.get(thread)
-  puts agent.page if opts[:verbose]
-rescue => e
-  puts "Cannot load the board page for the first time. Is #{opts[:site]} up, and are you online?"
-  Kernel.exit 1
-end
-
-#Slice up the files array
-opts[:maxfiles] - 1 if opts[:maxfiles] > 1
-posts = files.each_slice(opts[:maxfiles]).to_a
-
-#Drop as many posts as necessary to pick up where we left off, if specified by the user
-posts.shift(opts[:resume] - 1) if opts[:resume]
-
-posts.each_with_index do |files, post|
-  sleep opts[:delay] if started
-
-  started = true
-  upload = Hash.new
-
-  files.each_with_index do |filename, index|
-    upload["file#{index}"] = File.new(filename)
-    puts "Post #{post + 1}/#{posts.size}: uploading " + filename
+  if opt[:nonimages]
+    files << path unless path =~ /.+?(thumbs.db|\.DS_Store)/i #Disregard crappy OS-specific files.
+  else
+    files << path if path =~ /.+\.(jpg|jpeg|png|gif|bmp)$/i
   end
+  puts "Found #{files.size} files to dump"
 
-  case
-  when opts[:body]
-    body = "#{opts[:body]}"
-  when opts[:count] 
-    body = "#{post + 1}/#{posts.size}"
-  end
+  files.shuffle! if opts[:random]
 
-  puts "Post body is: #{body}" if opts[:verbose]
-  puts upload if opts[:verbose]
+  #Web interaction
+  agent = Mechanize.new
+  agent.user_agent = opts[:useragent]
+  #If you are having trouble dumping on a site that isn't 8chan, this is probably part of
+  #what needs to be modified. This should be the same for all vichan boards, but it absolutely
+  #won't work on Futaba-likes.
+  thread = opts[:site] + opts[:board] + "/res/" + opts[:thread] + ".html"
+  posthandler = opts[:site] + "post.php"
+
+  puts "Thread is: #{thread}" if opts[:verbose]
+  puts "User-agent is #{opts[:useragent]}" if opts[:verbose]
+  puts "Password is: #{opts[:password]}" if opts[:verbose]
+  puts "Files per post: #{opts[:maxfiles]}" if opts[:verbose]
+  puts "Name is #{opts[:name]}" if opts[:verbose]
+  puts "Email is #{opts[:email]}" if opts[:verbose]
+  puts "Subject is #{opts[:subject]}" if opts[:verbose]
+
+  errors = 0 #running error count
+  started = false #don't wait the first time
+
+  puts "Starting run with #{opts[:delay]} seconds between posts"
+  puts "===== " + Time.now.to_s + " ====="
 
   begin
-    #DO EET!
-    page = agent.post(posthandler, {
-      :post => 'New Reply',
-      :thread => opts[:thread],
-      :board => opts[:board],
-      :name => opts[:name],
-      :email => opts[:email],
-      :subject => opts[:subject],
-      :password => opts[:password],
-      :user_flag => opts[:userflag],
-      :body => body,
-      :file => upload["file0"],
-      :file1 => upload["file1"],
-      :file2 => upload["file2"],
-      :file3 => upload["file3"],
-      :file4 => upload["file4"],
-      :json_response => 1
-    })
-
-    #TODO: parse json for anything useful
-    puts page.body if opts[:verbose]
+    agent.get(thread)
+    puts agent.page if opts[:verbose]
   rescue => e
-    errors +=1
-    if errors < opts[:maxfailures]
-      puts e if opts[:verbose]
-      puts "Error - backing off for #{opts[:errordelay]} more seconds"
-    sleep opts[:errordelay]
-    puts "#{opts[:maxfailures] - errors} retries left."
-    redo
-    else 
-     puts "Hit max error limit! Bailing out at post #{post}"
-     puts "You may pick up where you left off by specifying this folder and --resume #{post} next time"
-     Kernel.exit 1
+    puts "Cannot load the board page for the first time. Is #{opts[:site]} up, and are you online?"
+    Kernel.exit 1
+  end
+
+  #Slice up the files array
+  opts[:maxfiles] - 1 if opts[:maxfiles] > 1
+  posts = files.each_slice(opts[:maxfiles]).to_a
+
+  #Drop as many posts as necessary to pick up where we left off, if specified by the user
+  posts.shift(opts[:resume] - 1) if opts[:resume]
+
+  posts.each_with_index do |files, post|
+    sleep opts[:delay] if started
+
+    started = true
+    upload = Hash.new
+
+    files.each_with_index do |filename, index|
+      upload["file#{index}"] = File.new(filename)
+      puts "Post #{post + 1}/#{posts.size}: uploading " + filename
+    end
+
+    case
+    when opts[:body]
+      body = "#{opts[:body]}"
+    when opts[:count] 
+      body = "#{post + 1}/#{posts.size}"
+    end
+
+    puts "Post body is: #{body}" if opts[:verbose]
+    puts upload if opts[:verbose]
+
+    begin
+      #DO EET!
+      page = agent.post(posthandler, {
+        :post => 'New Reply',
+        :thread => opts[:thread],
+        :board => opts[:board],
+        :name => opts[:name],
+        :email => opts[:email],
+        :subject => opts[:subject],
+        :password => opts[:password],
+        :user_flag => opts[:userflag],
+        :body => body,
+        :file => upload["file0"],
+        :file1 => upload["file1"],
+        :file2 => upload["file2"],
+        :file3 => upload["file3"],
+        :file4 => upload["file4"],
+        :json_response => 1
+      })
+
+      #TODO: parse json for anything useful
+      puts page.body if opts[:verbose]
+    rescue => e
+      errors +=1
+      if errors < opts[:maxfailures]
+        puts e if opts[:verbose]
+        puts "Error - backing off for #{opts[:errordelay]} more seconds"
+        sleep opts[:errordelay]
+        puts "#{opts[:maxfailures] - errors} retries left."
+        redo
+      else 
+        puts "Hit max error limit! Bailing out at post #{post}"
+        puts "You may pick up where you left off by specifying this folder and --resume #{post} next time"
+        Kernel.exit 1
+      end
     end
   end
 end
 
+puts "===== " + Time.now.to_s + " ====="
 puts "Work completed. Hail hotwheels!"
-Kernel.exit
